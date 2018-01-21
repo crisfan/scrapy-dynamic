3aefce57b60c4277e9e366d7009c067bcb369c2a
crisfan <13558798397@163.com>
2018-01-21 23:32:32 +0800

    optimize the code of general spider

--- crawler/crawling/spiders/general.py
+++ crawler/crawling/spiders/general.py
@@ -11,6 +11,11 @@ from crawling.spiders.redis_spider import RedisSpider
 from urlparse import urljoin
 
 
+def acquire(data):
+    return data[0] if data else 'the css of this field seems to be wrong'
+
+
+
 class ZzhSpider(RedisSpider):
     name = "zzh"
 
@@ -20,40 +25,31 @@ class ZzhSpider(RedisSpider):
     def parse_inform_index(self, response):
         """extract the links from notification list"""
 
-        if response.status != 200:
-            with open("log/error.txt", 'a') as f:
-                f.write('render error:' + response.url + response.status + '\n')
+        first_page_css = response.meta['first_page_css']  # 获取首页的字段及其规则
+        second_page_url = ''
 
-        field_css = response.meta
-
-        detail_url = 'http://404.html'
         try:
             selector = Selector(response)
 
-            for sel in selector.css(field_css['from']):  # 遍历列表项
+            for sel in selector.css(first_page_css.pop('list_from')):  # 遍历列表项
                 item = dict()
-                for field, val in field_css.items():
-                    if field in ['from', 'detail', 'continue', 'download_timeout', 'download_latency', 'download_slot']:
+                for field, val in first_page_css.items():
+                    if field == 'second_page_url':  # 提取详情页链接
+                        second_page_url = item[field] = urljoin(response.url, acquire(sel.css(val).extract()))
                         continue
 
-                    if field == 'href':  # 提取详情页链接
-                        detail_url = item[field] = urljoin(response.url, self.acquire(sel.css(val).extract()))
-                        continue
+                    # 提取首页用户自定义采集数据
+                    item[field] = acquire(sel.css(val).extract()) \
+                        if 'attr' in val else acquire(sel.css(val).xpath('string(.)').extract())  # 提取标签属性值或文本值
 
-                    item[field] = self.acquire(sel.css(val).extract()) \
-                        if 'attr' in val else self.acquire(sel.css(val).xpath('string(.)').extract())  # 提取标签属性值或文本值
 
-                con = field_css['continue']
-
-                if con == 'false':
+                if 'second_page_css' not in response.meta['second_page_css']:
                     yield item
 
-                if detail_url != 'http://404.html' and con == 'true':
-                    print 'c'
-                    yield SplashRequest(url=detail_url, meta={'item': item, 'detail': field_css['detail']},
+                if second_page_url:
+                    yield SplashRequest(url=second_page_url, meta={'item': item, 'second_page_css': response.meta['second_page_css']},
                                         callback=self.parse_inform_detail, dont_filter=False)  # 请求详情页面
         except Exception, e:
-            print e
             with open("log/error.txt", 'a') as f:
                 f.write('parse index error' + response.url + '\n')
 
@@ -63,10 +59,11 @@ class ZzhSpider(RedisSpider):
     def parse_inform_detail(self, response):
         """extract details of notification"""
 
-        field_css = response.meta['detail']
-        item = response.meta['item']
+        second_page_css = response.meta['second_page_css']
+        item = response.meta['item']  # 首页采集的数据
 
-        if field_css['general'] == 'true':
+        # 提取详情页正文部分
+        if 'body' in second_page_css:
             try:
                 content = Readability(response.body, response.url).content  # 提取网页正文
                 item['content'] = content  # content -> utf-8
@@ -75,8 +72,9 @@ class ZzhSpider(RedisSpider):
                     f.write("parse content error:" + response.url + '\n')
             return item
 
-        for field, val in field_css.items():
-            item[field] = self.acquire(response.css(val).extract())  # 提取详情页字段数据
+        # 提取详情页用户自定义数据
+        for field, val in second_page_css.items():
+            item[field] = acquire(response.css(val).extract())  # 提取详情页字段数据
 
         # get files' urls and store Files
         document = etree.HTML(response.body, parser=etree.HTMLParser(encoding='utf-8'))
@@ -86,6 +84,3 @@ class ZzhSpider(RedisSpider):
             item['exists_file'] = True
         return item
 
-    @staticmethod
-    def acquire(data):
-        return data[0] if data else 'the css of this field seems to be wrong'
